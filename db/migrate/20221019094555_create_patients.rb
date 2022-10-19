8055e2369e15a14a438ca45925aa696b2082d081
MarkBrianK <kariukimarkbrian@gmail.com>
2022-10-19 12:51:18 +0300

    Added the patient resources

---
+++ db/migrate/20221019094555_create_patients.rb
@@ -0,0 +1,19 @@
+class CreatePatients < ActiveRecord::Migration[7.0]
+  def change
+    create_table :patients do |t|
+      t.string :patient_name
+      t.date :reg_date
+      t.date :birth_date
+      t.string :gender
+      t.string :contact_no
+      t.string :email
+      t.string :guardian
+      t.string :patient_status
+      t.string :image
+      t.integer :doc_id
+      t.string :ticket_id
+
+      t.timestamps
+    end
+  end
+end

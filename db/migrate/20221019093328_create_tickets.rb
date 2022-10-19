e094d15ab66751da6536473fa35ecc373af6526d
= <=>
2022-10-19 14:41:38 +0300

    generate ticket resource

---
+++ db/migrate/20221019093328_create_tickets.rb
@@ -0,0 +1,11 @@
+class CreateTickets < ActiveRecord::Migration[7.0]
+  def change
+    create_table :tickets do |t|
+      t.integer :patient_id
+      t.integer :ticket_number
+      t.string :ticket_item
+
+      t.timestamps
+    end
+  end
+end

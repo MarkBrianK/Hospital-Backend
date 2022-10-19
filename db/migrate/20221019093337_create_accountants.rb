2f9011ef5191a8a3ecfb16b6d1d955ed45f1042f
SOS <sospeterbrian029@gmail.com>
2022-10-19 12:52:47 +0300

    creating accountant table

---
+++ db/migrate/20221019093337_create_accountants.rb
@@ -0,0 +1,10 @@
+class CreateAccountants < ActiveRecord::Migration[7.0]
+  def change
+    create_table :accountants do |t|
+      t.integer :patient_id
+      t.integer :cost
+
+      t.timestamps
+    end
+  end
+end

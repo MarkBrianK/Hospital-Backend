0e5cc954d080834feb5ebcf41ab06ed2e7af403c
Ian Muisyo Ngila <ngilaian@gmail.com>
2022-10-19 14:25:38 +0300

    setup db

--- db/migrate/20221019110015_create_pharmacies.rb
+++ db/migrate/20221019110015_create_pharmacies.rb
@@ -2,7 +2,7 @@ class CreatePharmacies < ActiveRecord::Migration[7.0]
   def change
     create_table :pharmacies do |t|
       t.string :patient_name
-      t.string :ticket_number
+      t.integer :ticket_id
       t.string :remarks
       t.string :inventory_item
 

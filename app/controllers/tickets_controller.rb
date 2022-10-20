92784e14043319f8570b54152cdaa3f121586bb8
Anthony Kimeu <tkymeu@gmail.com>
2022-10-20 09:13:58 +0300

    updated tickets controller

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -1,21 +1,52 @@
 class TicketsController < ApplicationController
     rescue_from ActiveRecord::RecordNotFound, with: :not_found_response
+   
     def index
-        render json: Ticket.all, status: :ok
+        all tickets = Ticket.all
+        render json: all_tickets
     end
+    
+
+
     def show
-        tickets = Ticket.find(params[:id])
-        render json: tickets, status: :ok
+        ticket = Ticket.find_by(id: params[:id])
+        if ticket_to_get
+            render json: ticket_to_get
+        else
+            render json: { error:"Ticket Not Found"}, status: :not_found
+        end
     end
+
+
 
     def create
-        ticket = Ticket.create!(ticket_params)
-        render json: ticket, status: :created
+        ticket = Ticket.create(ticket_params)
+        if
+            ticket_to_get.valid?
+            render json:ticket_to_create, status: :created
+        else
+            render json: {error: ticket_to_create.errors}, status: unprocessable_entity
+        end
     end
 
+
     def destroy
-        tickets = Ticket.find(params[:id])
-        tickets.destroy
-        head :no_content
-    end 
+        ticket_to_delete = Ticket.find_by(id: params[:id])
+        if
+            ticket_to_delete.valid?
+            ticket_to_delete.destroy 
+            render json:  {message: "deleted successfully"} 
+            head :no_content
+        else
+            render json: {error: "requested resource not found"}, status: not_found
+        end
+    end
+
+
+    private
+
+    def ticket_params
+        params.permit(:patient_id, :ticket_number, :ticket_item)
+
+    end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -22,8 +22,9 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_19_121639) do
   end
 
   create_table "doctors", force: :cascade do |t|
-    t.string "specializtion"
-    t.integer "reg_no"
+    t.string "name"
+    t.string "specialization"
+    t.integer "reg_number"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
@@ -46,15 +47,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_19_121639) do
     t.string "image"
     t.integer "doctor_id"
     t.integer "ticket_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "pharmacies", force: :cascade do |t|
-    t.string "patient_name"
-    t.integer "ticket_id"
-    t.string "remarks"
-    t.string "inventory_item"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end

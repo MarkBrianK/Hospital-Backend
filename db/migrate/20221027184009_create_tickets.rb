d0ae05503fd97a18318182e2e58504a77d477b9c
Anthony Kimeu <tkymeu@gmail.com>
2022-10-27 22:05:12 +0300

    milestone:added fields to ticket and lab tables

--- db/migrate/20221027184009_create_tickets.rb
+++ db/migrate/20221027184009_create_tickets.rb
@@ -1,6 +1,12 @@
 class CreateTickets < ActiveRecord::Migration[7.0]
   def change
     create_table :tickets do |t|
+      t.string :patient_name
+      t.string :inventory_item
+      t.string :item
+      t.string :discount
+      t.string :total_amount
+      t.string :receipt_no
 
       t.timestamps
     end

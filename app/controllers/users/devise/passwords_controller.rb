ab09d2975c85e819e7ea8074f3302922ed058520
= <=>
2022-10-27 14:11:59 +0300

    fix password reset

--- app/controllers/users/devise/passwords_controller.rb
+++ app/controllers/users/devise/passwords_controller.rb
@@ -27,8 +27,8 @@ class Users::Devise::PasswordsController < Devise::PasswordsController
   #   super(resource)
   # end
 
-  The path used after sending reset password instructions
-  def after_sending_reset_password_instructions_path_for(resource_name)
-    super(resource_name)
-  end
+  # The path used after sending reset password instructions
+  # def after_sending_reset_password_instructions_path_for(resource_name)
+  #   super(resource_name)
+  # end
 end

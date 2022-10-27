28e5a3ec27b735837599781ee476a10bd3d4899b
= <=>
2022-10-27 14:10:28 +0300

    update passwords

--- app/controllers/users/devise/passwords_controller.rb
+++ app/controllers/users/devise/passwords_controller.rb
@@ -1,10 +1,10 @@
 # frozen_string_literal: true
 
 class Users::Devise::PasswordsController < Devise::PasswordsController
-  # GET /resource/password/new
-  # def new
-  #   super
-  # end
+  GET /resource/password/new
+  def new
+    super
+  end
 
   # POST /resource/password
   # def create

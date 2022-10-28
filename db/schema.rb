a7776b1cf555adc034ca59aea8f4a9825078b7c9
SOS <sospeterbrian029@gmail.com>
2022-10-28 12:07:05 +0300

    migrating pharmacy

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_27_184412) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_28_085417) do
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -69,6 +69,15 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_27_184412) do
     t.string "relation"
     t.string "gender"
     t.string "patient_status"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "pharmacies", force: :cascade do |t|
+    t.integer "patient_id"
+    t.integer "ticket_id"
+    t.string "remarks"
+    t.string "inventory_item"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
